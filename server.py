dd88135e9b67f812a15902e9565b9e4d6740b5ba
Kozeeva Anna <kozeeva.an@yandex.ru>
2020-03-21 01:24:11 +0300

    cors: Implemented sending a json to server

--- server.py
+++ server.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, make_response
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
+
 def index():
     return render_template('index.html')
 
+
+@app.route('/friends', methods=['POST'])
+
+def friends():
+    req = request.get_json()
+    response = make_response(jsonify(req), 200)
+    return response
+
+
 if __name__ == '__main__':
     app.run()
+
