ba43687ba88c26f3e83cf2570cfeabf72dc99659
Kozeeva Anna <kozeeva.an@yandex.ru>
2020-03-21 16:44:31 +0300

    cors: parameters for CORS added

--- server.py
+++ server.py
@@ -2,16 +2,17 @@ from flask import Flask, render_template, request, jsonify, make_response
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+cors = CORS(app, 
+            origins = ['http://127.0.0.1:5000', 'http://82.148.17.124/', 'http://applotus.ru'],
+            methods = ['GET', 'POST'])
+
 
 @app.route('/')
-
 def index():
     return render_template('index.html')
 
 
 @app.route('/friends', methods=['POST'])
-
 def friends():
     req = request.get_json()
     response = make_response(jsonify(req), 200)
